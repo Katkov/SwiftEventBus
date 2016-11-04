fd6e0d87beb9f5053bafc1cf1f969dcba11fa0e4
Michael Katkov <eloorus@gmail.com>
2016-11-04 09:55:50 -0200

    version for Swift 2.3

--- SwiftEventBus/SwiftEventBus.swift
+++ SwiftEventBus/SwiftEventBus.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-open class SwiftEventBus {
+class SwiftEventBus {
     
     struct Static {
         static let instance = SwiftEventBus()
-        static let queue = DispatchQueue(label: "com.cesarferreira.SwiftEventBus", attributes: [])
+        static let queue = dispatch_queue_create("com.cesarferreira.SwiftEventBus", DISPATCH_QUEUE_CONCURRENT)
     }
     
     struct NamedObserver {
@@ -19,69 +19,68 @@ open class SwiftEventBus {
     // Publish
     ////////////////////////////////////
     
-    open class func post(_ name: String) {
-        NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: nil)
+    class func post(name: String) {
+        NSNotificationCenter.defaultCenter().postNotificationName(name, object: nil)
     }
-    
-    open class func post(_ name: String, sender: AnyObject?) {
-        NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: sender)
+        
+    class func post(name: String, sender: AnyObject?) {
+        NSNotificationCenter.defaultCenter().postNotificationName(name, object: sender)
     }
-    
-    open class func post(_ name: String, sender: NSObject?) {
-        NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: sender)
+        
+    class func post(name: String, sender: NSObject?) {
+        NSNotificationCenter.defaultCenter().postNotificationName(name, object: sender)
     }
-    
-    open class func post(_ name: String, userInfo: [AnyHashable: Any]?) {
-        NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: nil, userInfo: userInfo)
+        
+    class func post(name: String, userInfo: [NSObject: AnyObject]?) {
+        NSNotificationCenter.defaultCenter().postNotificationName(name, object: nil, userInfo: userInfo)
     }
-    
-    open class func post(_ name: String, sender: AnyObject?, userInfo: [AnyHashable: Any]?) {
-        NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: sender, userInfo: userInfo)
+        
+    class func post(name: String, sender: AnyObject?, userInfo: [NSObject: AnyObject]?) {
+        NSNotificationCenter.defaultCenter().postNotificationName(name, object: sender, userInfo: userInfo)
     }
-    
-    open class func postToMainThread(_ name: String) {
-        DispatchQueue.main.async {
-            NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: nil)
+        
+    class func postToMainThread(name: String) {
+        dispatch_async(Static.queue) {
+            NSNotificationCenter.defaultCenter().postNotificationName(name, object: nil)
         }
     }
-    
-    open class func postToMainThread(_ name: String, sender: AnyObject?) {
-        DispatchQueue.main.async {
-            NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: sender)
+        
+    class func postToMainThread(name: String, sender: AnyObject?) {
+        dispatch_async(Static.queue) {
+            NSNotificationCenter.defaultCenter().postNotificationName(name, object: sender)
         }
     }
-    
-    open class func postToMainThread(_ name: String, sender: NSObject?) {
-        DispatchQueue.main.async {
-            NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: sender)
+        
+    class func postToMainThread(name: String, sender: NSObject?) {
+        dispatch_async(Static.queue) {
+            NSNotificationCenter.defaultCenter().postNotificationName(name, object: sender)
         }
     }
-    
-    open class func postToMainThread(_ name: String, userInfo: [AnyHashable: Any]?) {
-        DispatchQueue.main.async {
-            NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: nil, userInfo: userInfo)
+        
+    class func postToMainThread(name: String, userInfo: [NSObject: AnyObject]?) {
+        dispatch_async(Static.queue) {
+            NSNotificationCenter.defaultCenter().postNotificationName(name, object: nil, userInfo: userInfo)
         }
     }
-    
-    open class func postToMainThread(_ name: String, sender: AnyObject?, userInfo: [AnyHashable: Any]?) {
-        DispatchQueue.main.async {
-            NotificationCenter.default.post(name: Notification.Name(rawValue: name), object: sender, userInfo: userInfo)
+        
+    class func postToMainThread(name: String, sender: AnyObject?, userInfo: [NSObject: AnyObject]?) {
+        dispatch_async(Static.queue) {
+            NSNotificationCenter.defaultCenter().postNotificationName(name, object: sender, userInfo: userInfo)
         }
     }
-    
-
-    
-    ////////////////////////////////////
-    // Subscribe
-    ////////////////////////////////////
-    
-    @discardableResult
-    open class func on(_ target: AnyObject, name: String, sender: AnyObject?, queue: OperationQueue?, handler: @escaping ((Notification!) -> Void)) -> NSObjectProtocol {
-        let id = UInt(bitPattern: ObjectIdentifier(target))
-        let observer = NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: name), object: sender, queue: queue, using: handler)
+        
+        
+        
+        ////////////////////////////////////
+        // Subscribe
+        ////////////////////////////////////
+        
+    class func on(target: AnyObject, name: String, sender: AnyObject?, queue: NSOperationQueue?, handler: ((NSNotification!) -> Void)) -> NSObjectProtocol {
+        let id = UInt(bitPattern: ObjectIdentifier(target).hashValue)
+        let observer = NSNotificationCenter.defaultCenter().addObserverForName(name, object: sender, queue: queue, usingBlock: handler)
         let namedObserver = NamedObserver(observer: observer, name: name)
         
-        Static.queue.sync {
+        dispatch_sync(Static.queue) {
             if let namedObservers = Static.instance.cache[id] {
                 Static.instance.cache[id] = namedObservers + [namedObserver]
             } else {
@@ -91,49 +90,45 @@ open class SwiftEventBus {
         
         return observer
     }
-    
-    @discardableResult
-    open class func onMainThread(_ target: AnyObject, name: String, handler: @escaping ((Notification!) -> Void)) -> NSObjectProtocol {
-        return SwiftEventBus.on(target, name: name, sender: nil, queue: OperationQueue.main, handler: handler)
+        
+    class func onMainThread(target: AnyObject, name: String, handler: ((NSNotification!) -> Void)) -> NSObjectProtocol {
+        return SwiftEventBus.on(target, name: name, sender: nil, queue: NSOperationQueue.mainQueue(), handler: handler)
     }
-    
-    @discardableResult
-    open class func onMainThread(_ target: AnyObject, name: String, sender: AnyObject?, handler: @escaping ((Notification!) -> Void)) -> NSObjectProtocol {
-        return SwiftEventBus.on(target, name: name, sender: sender, queue: OperationQueue.main, handler: handler)
+        
+    class func onMainThread(target: AnyObject, name: String, sender: AnyObject?, handler: ((NSNotification!) -> Void)) -> NSObjectProtocol {
+        return SwiftEventBus.on(target, name: name, sender: sender, queue: NSOperationQueue.mainQueue(), handler: handler)
     }
-    
-    @discardableResult
-    open class func onBackgroundThread(_ target: AnyObject, name: String, handler: @escaping ((Notification!) -> Void)) -> NSObjectProtocol {
-        return SwiftEventBus.on(target, name: name, sender: nil, queue: OperationQueue(), handler: handler)
+        
+    class func onBackgroundThread(target: AnyObject, name: String, handler: ((NSNotification!) -> Void)) -> NSObjectProtocol {
+        return SwiftEventBus.on(target, name: name, sender: nil, queue: NSOperationQueue(), handler: handler)
     }
-    
-    @discardableResult
-    open class func onBackgroundThread(_ target: AnyObject, name: String, sender: AnyObject?, handler: @escaping ((Notification!) -> Void)) -> NSObjectProtocol {
-        return SwiftEventBus.on(target, name: name, sender: sender, queue: OperationQueue(), handler: handler)
+        
+    class func onBackgroundThread(target: AnyObject, name: String, sender: AnyObject?, handler: ((NSNotification!) -> Void)) -> NSObjectProtocol {
+        return SwiftEventBus.on(target, name: name, sender: sender, queue: NSOperationQueue(), handler: handler)
     }
-    
+        
     ////////////////////////////////////
     // Unregister
     ////////////////////////////////////
-    
-    open class func unregister(_ target: AnyObject) {
-        let id = UInt(bitPattern: ObjectIdentifier(target))
-        let center = NotificationCenter.default
         
-        Static.queue.sync {
-            if let namedObservers = Static.instance.cache.removeValue(forKey: id) {
+    class func unregister(target: AnyObject) {
+        let id = UInt(bitPattern: ObjectIdentifier(target).hashValue)
+        let center = NSNotificationCenter.defaultCenter()
+        
+        dispatch_sync(Static.queue) {
+            if let namedObservers = Static.instance.cache.removeValueForKey(id) {
                 for namedObserver in namedObservers {
                     center.removeObserver(namedObserver.observer)
                 }
             }
         }
     }
-    
-    open class func unregister(_ target: AnyObject, name: String) {
-        let id = UInt(bitPattern: ObjectIdentifier(target))
-        let center = NotificationCenter.default
         
-        Static.queue.sync {
+    class func unregister(target: AnyObject, name: String) {
+        let id = UInt(bitPattern: ObjectIdentifier(target).hashValue)
+        let center = NSNotificationCenter.defaultCenter()
+        
+        dispatch_sync(Static.queue) {
             if let namedObservers = Static.instance.cache[id] {
                 Static.instance.cache[id] = namedObservers.filter({ (namedObserver: NamedObserver) -> Bool in
                     if namedObserver.name == name {
@@ -146,5 +141,5 @@ open class SwiftEventBus {
             }
         }
     }
-    
+        
 }
